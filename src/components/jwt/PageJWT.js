71f8565a3e9fd0740e4a2c60c234822fdb313c48
Eli Sherer <eli.sherer@gmail.com>
2019-02-22 17:08:53 +0200

    better jwt error handling

--- src/components/jwt/PageJWT.js
+++ src/components/jwt/PageJWT.js
@@ -1,6 +1,5 @@
 import { h } from 'hyperapp';
 import cc from 'classcat';
-import {Link, Redirect} from '@hyperapp/router';
 import TextBox from '../TextBox';
 import TextArea from '../TextArea';
 import Tabs from '../Tabs';
@@ -10,51 +9,74 @@ import CopyToClipboard from "../CopyToClipboard";
 
 export default () => (state, actions) => {
   const encodeMode = state.jwt.encode;
-  let resultHTML;
 
-  resultHTML =
-    `<div class="${styles.header}">${state.jwt.header || ''}</div>` +
-    `<div class="${styles.payload}">${state.jwt.payload || ''}</div>` +
-    `<div class="${styles.sig}">${state.jwt.sig || ''}</div>`;
+  const tabs = (
+    <Tabs>
+      <span data-active={!encodeMode} onclick={actions.jwt.toggle}>Decoder</span>
+      <span data-active={encodeMode} onclick={actions.jwt.toggle}>Encoder</span>
+    </Tabs>
+  );
 
   const secretTextbox = <TextBox value={state.jwt.secret}
                                  placeholder="Base64Url encoded secret"
                                  onChange={actions.jwt.secret} />;
-  return (
-    <div>
-      <Tabs>
-        <span data-active={!encodeMode} onclick={actions.jwt.toggle}>Decoder</span>
-        <span data-active={encodeMode} onclick={actions.jwt.toggle}>Encoder</span>
-      </Tabs>
 
-      {encodeMode && (<div>
+  if (encodeMode) {
+    return (
+      <div>
+        {tabs}
+
         <label>Algorithm</label>
         <TextBox value="HS256" readonly/>
 
         <TextArea className={styles.header} value={state.jwt.header} readonly />
         <TextArea className={styles.payload} value={state.jwt.in_payload} onChange={actions.jwt.in_payload} autofocus />
-        <TextArea className={styles.sig} value={state.jwt.sig} readonly />
+        <TextArea className={styles.sig} value={state.jwt.secret ? state.jwt.sig : ''} readonly />
 
         <label>Secret key</label>
         {secretTextbox}
-      </div>)}
 
-      <span>Token</span><CopyToClipboard from="jwt" />
-      <TextBox id="jwt" style={{maxWidth: "100%"}} autofocus={!encodeMode}
-               value={encodeMode ? state.jwt.out_token : state.jwt.in_token}
-               selectOnFocus
-               onChange={actions.jwt.in_token}
-               readonly={encodeMode} />
+        {state.jwt.error
+          ? <p style={{color: 'red'}}>{state.jwt.error}</p> : (
+            <div>
+              <span>Token</span><CopyToClipboard from="jwt"/>
+              <TextBox id="jwt"
+                       className={cc([styles.token, { [styles.error]: state.jwt.error }])}
+                       value={state.jwt.out_token}
+                       selectOnFocus
+                       readonly />
+            </div>
+          )
+        }
+      </div>
+    );
+  }
 
-      {!encodeMode && (<div>
-        { state.jwt.alg && (
-          <label className={styles.emoji}>Verify <b>{state.jwt.alg}</b> Signature {state.jwt.valid ? "- ✔ Verified" : "- ❌ Not verified"}</label>)}
-        {state.jwt.alg && secretTextbox}
+  const resultHTML =
+    `<div class="${styles.header}">${state.jwt.header || ''}</div>` +
+    `<div class="${styles.payload}">${state.jwt.payload || ''}</div>` +
+    `<div class="${styles.sig}">${state.jwt.sig || ''}</div>`;
 
-        <label>Contents:</label>
-        <TextArea readonly={!encodeMode} html className={styles.jwt}
-                  value={resultHTML} />
-      </div>)}
+  return (
+    <div>
+      {tabs}
+
+      <span>Token</span><CopyToClipboard from="jwt"/>
+      <TextBox id="jwt" autofocus selectOnFocus
+               className={cc([styles.token, { [styles.error]: state.jwt.error }])}
+               value={state.jwt.in_token}
+               onChange={actions.jwt.in_token} />
+      { state.jwt.error && (
+        <p style={{color: 'red'}}>{state.jwt.error}</p>
+      )}
+      { state.jwt.alg && (
+        <label className={styles.emoji}>Verify <b>{state.jwt.alg}</b> Signature {state.jwt.valid ? "- ✔ Verified" : "- ❌ Not verified"}</label>
+      )}
+      {state.jwt.alg && secretTextbox}
+
+      <label>Contents:</label>
+      <TextArea readonly={!encodeMode} html className={styles.jwt}
+                value={resultHTML} />
     </div>
   );
 }
--- src/components/jwt/jwt.js
+++ src/components/jwt/jwt.js
@@ -30,34 +30,28 @@ const arrayToBase64Url = a => base64UrlEncodeWeb(String.fromCharCode(...new Uint
 
 
 const base64UrlDecode = b64uval => {
-  if (!b64uval.length) return b64uval;
   try {
+    if (!b64uval || !b64uval.length) return b64uval;
     const b64val = b64uval.replace(/-/g, '+').replace(/_/g, '/');
-    return JSON.parse(atob(b64val));
+    return atob(b64val);
   }
-  catch (e)
-  {
-    console.error(e); // eslint-disable-line
-    return 'Error';
+  catch(e) {
+    return {
+      error: 'Bad Base64Url value'
+    };
   }
 };
 
 const sign = async (alg, token, secret) => {
-  try {
-    if (secret && secret.length > 1) {
-      switch (alg) {
-        case 'HS256': {
-          const array = await hmacSha256(token, secret);
-          return arrayToBase64Url(array);
-        }
+  if (secret && secret.length > 1) {
+    switch (alg) {
+      case 'HS256': {
+        const array = await hmacSha256(token, secret);
+        return arrayToBase64Url(array);
       }
     }
-    return '';
   }
-  catch (e) {
-    console.error(e); // eslint-disable-line
-    return '';
-  }
+  return '';
 };
 
 const encodeAsync = async (alg, payload, key) => {
@@ -75,26 +69,49 @@ const encodeAsync = async (alg, payload, key) => {
     const prefix = encodedHeader + '.' + encodedPayload;
     const sig = await sign(alg, prefix, key);
 
-    return {out_token: prefix + '.' + sig, sig};
+    return {out_token: prefix + '.' + sig, sig, error: false};
   }
   catch (e) {
-    return { out_token: e.message};
+    return { error: e.message};
   }
 };
 
+const parseAndPrettify = input => {
+  if (typeof input !== 'string') return input; // something is wrong
+  try {
+    const obj = JSON.parse(input);
+    return {
+      pretty: JSON.stringify(obj, null, 2),
+      obj
+    };
+  }
+  catch (e) {
+    return {
+      pretty: input,
+    };
+  }
+};
 
 const decodeAsync = async (token, secret) => {
+  const result = { alg: '', header: '', payload: '', sig: '', valid: false, error: false };
   try {
-    const parts = token.split('.'), result = { alg: '', header: '', payload: '', sig: '', valid: false };
+    const parts = token.split('.');
     if (parts.length > 0) {
-      const header = base64UrlDecode(parts[0]);
-      result.header = JSON.stringify(header, null, 2);
-
+      const parsedHeader = parseAndPrettify(base64UrlDecode(parts[0]));
+      const header = parsedHeader.obj;
+      result.header = parsedHeader.pretty;
+      if (!parsedHeader.obj) {
+        result.error = "Invalid header";
+      }
       if (parts.length > 1) {
-        result.payload = JSON.stringify(base64UrlDecode(parts[1]), null, 2);
+        const parsedPayload = parseAndPrettify(base64UrlDecode(parts[1]));
+        result.payload = parsedPayload.pretty;
+        if (parsedPayload.error) {
+          result.error = parsedPayload.error;
+        }
         result.sig = parts.length > 2 ? parts[2] : '';
         let sig = 'No alg!';
-        if (header.alg && header.alg.toLowerCase() !== "none") {
+        if (header && header.alg && header.alg.toLowerCase() !== "none") {
           result.alg = header.alg;
           sig = await sign(header.alg, `${parts[0]}.${parts[1]}`, secret);
         }
@@ -104,8 +121,8 @@ const decodeAsync = async (token, secret) => {
     return result;
   }
   catch (e) {
-    console.error(e); // eslint-disable-line
-    return [];
+    result.error = e.message;
+    return result;
   }
 };
 
