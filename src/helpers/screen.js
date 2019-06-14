b1be4db974d9fcba0b354c9bff1bacdeace23e61
Eli Sherer <eli.sherer@gmail.com>
2019-06-14 13:43:51 +0300

    fix for Safari didn't work, remove matchMedia listener on safari

--- src/helpers/screen.js
+++ src/helpers/screen.js
@@ -2,11 +2,21 @@ const screen = {
   isDesktop: null
 };
 
-const screen_desktop_min = "600px";
+const isSafari = () => {
+  const ua = navigator.userAgent.toLowerCase();
+  return ua.indexOf('safari') !== -1 && ua.indexOf('chrome') === -1
+};
 
-const handleMobileDesktopSwitch = x => screen.isDesktop = x.matches;
-const isDesktopMatcher = window.matchMedia(`screen and (min-width: ${screen_desktop_min})`);
-handleMobileDesktopSwitch(isDesktopMatcher);
-isDesktopMatcher.addEventListener('change', handleMobileDesktopSwitch);
+const screen_desktop_min = 600,
+  screen_desktop_min_px = screen_desktop_min + "px";
+
+if (isSafari()) { // doesn't work on Safari
+  screen.isDesktop = window.screen.width < screen_desktop_min;
+} else {
+  const handleMobileDesktopSwitch = x => screen.isDesktop = x.matches;
+  const isDesktopMatcher = window.matchMedia(`screen and (min-width: ${screen_desktop_min_px})`);
+  handleMobileDesktopSwitch(isDesktopMatcher);
+  isDesktopMatcher.addEventListener('change', handleMobileDesktopSwitch);
+}
 
 export default screen;
