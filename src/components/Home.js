8097f1ef9ab1576cbc249ee6cd726bdf84034c2d
Eli Sherer <eli.sherer@gmail.com>
2019-07-07 20:21:09 +0300

    add joke of the day

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import jokes from '../jokes';
+
 import styles from "./Home.less";
 
 import logo from '../assets/devek_text.svg';
+
+const dayOfTheYear = ~~((Date.now() - (new Date(new Date().getFullYear() + '-01-01'))) / 864e5);
+const jokeOfTheDay = jokes[(dayOfTheYear % jokes.length)];
 
 const Home = () => (
   <div className={styles.page}>
@@ -11,6 +16,11 @@ const Home = () => (
       <strong>Everything</strong> is done on the client side,<br/>
       your information is <strong>safe</strong>!<br/>
     </p>
+
+    <div className={styles.joke_header}>🤣 Joke of the day:</div>
+    <div className={styles.joke}>
+      {jokeOfTheDay}
+    </div>
 
     <p>Press <kbd>/</kbd> for quick search</p>
 
---
+++ src/jokes.js
@@ -0,0 +1,28 @@
+export default [
+
+  `Knock knock.
+Race condition.
+Who's there?`,
+
+  `I've got a really good UDP joke to tell you, but I don't know if you'll get it.`,
+
+  `Q: How many programmers does it take to screw in a light bulb?
+A: None. It's a hardware problem.`,
+
+  `Why C gets all the chicks and Java doesn't? Because C doesn't treat them like objects.`,
+
+  `Why do Java programmers wear glasses? Because they can't C#.`,
+
+  `["hip", "hip"]`,
+
+  `I can change the world! But first I need the source code.`,
+
+  `Declare variables, not war. Execute programs, not people.`,
+
+  `99 bugs exists on the code, 99 bugs exists.
+Take one down and fix it at once. 100 bugs exists on the code.`,
+
+  `There are 10 types of people in the world, those who understand binary, and those who don't.`,
+
+  `!false - It's funny because it's true.`,
+];
